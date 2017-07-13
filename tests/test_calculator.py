ad6cae77f04f4d01365faa428c74b8c9659af5c0
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-07-13 13:39:07 -0700

    TST: Added find_combinations test for calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -16,19 +16,23 @@ from transfocate.calculator import Calculator
 @pytest.fixture(scope='module')
 def calculator():
     #Define prefocus lenses
-    prefocus = [Lens(300, 100, 100),
-                Lens(200, 100, 50)]
+    prefocus = [Lens(500., 125.0, 100.),
+                Lens(500., 145.0, 200.)]
     #Define transfocator
-    tfs = [Lens(300, 200, 25),
-           Lens(200, 100, 20)]
+    tfs = [Lens(500., 100., 50.),
+           Lens(500., 275., 25.)]
     #Define Calculator
     return Calculator(xrt_lenses = prefocus,
                       tfs_lenses = tfs,
                       xrt_limit  = 75,
                       tfs_limit  = 18)
 
-
 def test_calculator_combinations(calculator):
     #Eight possible transfocator combinations
     #Three possible prefocus lens choices
     assert len(calculator.combinations) == 24
+
+def test_calculator_find_combinations(calculator):
+    solutions = calculator.find_combinations(312.5, num_sol=1)
+    #Assert we found the accurate combination
+    assert np.isclose(solutions[0].image(0.0), 312.5, atol=0.1)
