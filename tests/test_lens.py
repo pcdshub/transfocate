0b3b998de4356e0c75736ab141666ac2e9c43d80
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-07-24 22:24:01 -0700

    TST: Added tests for motion handling

--- tests/test_lens.py
+++ tests/test_lens.py
@@ -5,23 +5,34 @@
 ###############
 # Third Party #
 ###############
-import pytest
 import numpy as np
 
 ##########
 # Module #
 ##########
 
-def test_lens_properties(lens, pyioc):
+def test_lens_properties(lens):
     assert np.isclose(500.0, lens.radius, atol=0.1)
     assert np.isclose(100.0, lens.z,      atol=0.1)
     assert np.isclose(50.0,  lens.focus,  atol=0.1)
 
-def test_image_from_obj(lens, pyioc):
+def test_image_from_obj(lens):
     #Real image
     assert np.isclose(lens.image_from_obj(0.0),  200.0, atol=0.1)
     #Imaginary image
     assert np.isclose(lens.image_from_obj(75.0), 50.0, atol=0.1)
+
+def test_lens_state_readback(lens):
+    lens.state.value = 0
+    assert lens.inserted == False
+    lens.state.value = 1
+    assert lens.inserted == True
+
+def test_lens_motion(lens):
+    lens.insert()
+    assert lens.in_signal.value == 1
+    lens.remove()
+    assert lens.out_signal.value == 1
 
 def test_lens_connect_effective_radius(array):
     assert np.isclose(array.effective_radius, 250, atol=0.1)
@@ -32,6 +43,7 @@ def test_lens_connect_image(array):
     assert np.isclose(array.image(80.0), 303.409, atol=0.1)
     assert np.isclose(array.image(125.0), 304.6875, atol=0.1)
 
-
 def test_number_of_lenses(array):
     assert array.nlens()== 2
+
+
