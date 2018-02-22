d5971e3288c529075e9e7a5c3ff43ee945b9bbbc
Teddy Rendahl <trendahl@slac.stanford.edu>
2018-02-21 22:31:24 -0800

    STY: PEP8 compliance

--- transfocate/tests/test_lens.py
+++ transfocate/tests/test_lens.py
@@ -12,31 +12,35 @@ import numpy as np
 ##########
 from pcdsdevices.sim.pv import using_fake_epics_pv
 
+
 @using_fake_epics_pv
 def test_lens_properties(lens):
     assert np.isclose(500.0, lens.radius, atol=0.1)
     assert np.isclose(100.0, lens.z,      atol=0.1)
     assert np.isclose(50.0,  lens.focus,  atol=0.1)
 
+
 @using_fake_epics_pv
 def test_image_from_obj(lens):
-    #Real image
+    # Real image
     assert np.isclose(lens.image_from_obj(0.0),  200.0, atol=0.1)
-    #Imaginary image
+    # Imaginary image
     assert np.isclose(lens.image_from_obj(75.0), 50.0, atol=0.1)
+
 
 @using_fake_epics_pv
 def test_lens_state(lens):
     # Inserted Lens
     lens._removed._read_pv.put(0)
     lens._inserted._read_pv.put(1)
-    assert lens.inserted == True
-    assert lens.removed == False
+    assert lens.inserted
+    assert not lens.removed
     # Removed Lens
     lens._removed._read_pv.put(1)
     lens._inserted._read_pv.put(0)
-    assert lens.removed == True
-    assert lens.inserted == False
+    assert lens.removed
+    assert not lens.inserted
+
 
 @using_fake_epics_pv
 def test_lens_motion(lens):
@@ -45,8 +49,10 @@ def test_lens_motion(lens):
     lens.remove()
     lens._remove._write_pv.get() == 1
 
+
 def test_lens_connect_effective_radius(array):
     assert np.isclose(array.effective_radius, 250, atol=0.1)
+
 
 def test_lens_connect_image(array):
     assert np.isclose(array.image(0.0),  312.5,   atol=0.1)
@@ -54,8 +60,10 @@ def test_lens_connect_image(array):
     assert np.isclose(array.image(80.0), 303.409, atol=0.1)
     assert np.isclose(array.image(125.0), 304.6875, atol=0.1)
 
+
 def test_number_of_lenses(array):
-    assert array.nlens== 2
+    assert array.nlens == 2
+
 
 def test_lens_sorting(array):
     assert array.lenses[0].z < array.lenses[1].z
