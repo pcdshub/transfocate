ac85aad6ec80e06a0c0190f3d72e47f030d26587
Teddy Rendahl <trendahl@slac.stanford.edu>
2018-02-22 12:45:31 -0800

    DOC: Clean IPython example

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ sys.path.insert(0,module_path)
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'IPython.sphinxext.ipython_directive',
+              'IPython.sphinxext.ipython_console_highlighting',
               'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
--- transfocate/tests/__init__.py
+++ transfocate/tests/__init__.py
@@ -1,3 +1,8 @@
-__all__ = ['transfocator']
+__all__ = ['test_transfocate']
 
-from .test_transfocate import transfocator
+from . import test_transfocate
+
+# Create a Transfocator with a few inserted lenses
+transfocator = test_transfocate.transfocator()
+test_transfocate.insert(transfocator.prefocus_mid)
+test_transfocate.insert(transfocator.tfs_02)
