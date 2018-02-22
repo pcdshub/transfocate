9cf97a23eb9cf13ebc219a9fd1a5bb5bb3010f9c
Teddy Rendahl <trendahl@slac.stanford.edu>
2018-02-22 10:17:05 -0800

    TST: Shorten and diversify transfocator tests

--- transfocate/tests/test_transfocate.py
+++ transfocate/tests/test_transfocate.py
@@ -1,130 +1,84 @@
-############
-# Standard #
-############
 import logging
 
-###############
-# Third Party #
-###############
 import pytest
+import numpy as np
 from pcdsdevices.sim.pv import using_fake_epics_pv
 
-##########
-# Module #
-##########
-from .conftest import FakeLens
 from transfocate.transfocator import Transfocator
-import numpy as np
+
+logger = logging.getLogger(__name__)
+
+
+# Utility functions for manipulating simulated lenses
+def insert(lens):
+    lens._removed._read_pv.put(0)
+    lens._inserted._read_pv.put(1)
+
+
+def remove(lens):
+    lens._removed._read_pv.put(1)
+    lens._inserted._read_pv.put(0)
+
 
 @pytest.fixture(scope='function')
 @using_fake_epics_pv
 def transfocator():
-    #Prefocus lenses
-    prefocus = [FakeLens(500., 100.0, 50.),
-                FakeLens(300., 100.0, 25.)]
-
-    #Define transfocator
-    tfs = [FakeLens(500., 275., 25.),
-           FakeLens(500., 280., 55.),
-           FakeLens(500., 300., 75.),
-           FakeLens(200., 310., 10.)]
-
-    trans = Transfocator("TST:TFS:LENS:",
-                         xrt_lenses=prefocus,
-                         tfs_lenses=tfs,
+    # Create our base transfocator
+    trans = Transfocator("TST:LENS",
                          name='Transfocator')
-    trans.xrt_limit._read_pv.put(200.0)
-    trans.tfs_limit._read_pv.put(750.0)
+    # Set all the lens values to ridiculous values and out
+    for lens in trans.xrt_lenses:
+        lens._sig_z._read_pv.put(100.0)
+        lens._sig_focus._read_pv.put(1000.0)
+        remove(lens)
+    for lens, z in zip(trans.tfs_lenses,
+                       np.linspace(250, 260, len(trans.tfs_lenses))):
+        lens._sig_z._read_pv.put(z)
+        lens._sig_focus._read_pv.put(1000.0)
+        remove(lens)
+    # Give two reasonable values so we can test calculations
+    trans.prefocus_bot._sig_focus._read_pv.put(50.0)
+    trans.tfs_02._sig_z._read_pv.put(275.)
+    trans.tfs_02._sig_focus._read_pv.put(25.)
+    # Use a nominal sample position
+    trans.nominal_sample = 300.0
+    # Set a reasonable limit
+    trans.xrt_limit._read_pv.put(-1.0)
     return trans
 
+
+@using_fake_epics_pv
 def test_transfocator_current_focus(transfocator):
-    #test with tfs[0] and xrt[0]
-    transfocator.xrt_lenses[0].insert()
-    transfocator.tfs_lenses[0].insert()
-    assert transfocator.current_focus == 312.5
-    
-    #test with xrt[0] and tfs[0,1]
-    transfocator.tfs_lenses[1].insert()
-    assert np.isclose(300.4, transfocator.current_focus, atol=0.1)
-    
-    #test with no xrt and tfs[0,1,2]
-    transfocator.xrt_lenses[0].remove()
-    transfocator.tfs_lenses[2].insert()
-    assert np.isclose(295.7, transfocator.current_focus, atol=0.1)
-    
-    #test with xrt[1] and tfs[1,3]
-    transfocator.xrt_lenses[1].insert()
-    transfocator.tfs_lenses[0].remove()
-    transfocator.tfs_lenses[2].remove()
-    transfocator.tfs_lenses[3].insert()
-    assert np.isclose(318.5, transfocator.current_focus, atol=0.1)
-    
-    #test with xrt[1] and tfs[1] 
-    transfocator.tfs_lenses[3].remove()
-    assert np.isclose(367.9, transfocator.current_focus, atol=0.1)
-    
-    #test xrt[1] and tfs[0]
-    transfocator.tfs_lenses[1].remove()
-    transfocator.tfs_lenses[0].insert()
-    assert np.isclose(305.35, transfocator.current_focus, atol=0.1)
-    
-    #test xrt[1] and tfs[0,1,2,3]
-    transfocator.tfs_lenses[1].insert()
-    transfocator.tfs_lenses[2].insert()
-    transfocator.tfs_lenses[3].insert()
-    assert np.isclose(356.48, transfocator.current_focus, atol=0.1)
+    assert np.isnan(transfocator.current_focus)
+    insert(transfocator.prefocus_bot)
+    insert(transfocator.tfs_02)
+    assert transfocator.tfs_02.inserted
+    assert transfocator.prefocus_bot.inserted
+    assert transfocator.current_focus == 12.5
 
+
+@using_fake_epics_pv
 def test_transfocator_find_best_combo(transfocator):
-    #test with tfs[0] and xrt[0]
-    assert transfocator.find_best_combo(312.5).nlens==2
-    assert np.isclose(250.0, transfocator.find_best_combo(312.5).effective_radius, atol=0.1)
-    assert np.isclose(312.5, transfocator.find_best_combo(312.5).image(0.0), atol=0.1)
-    
-    #test with xrt[1] and tfs[1,3]
-    assert transfocator.find_best_combo(318.5).nlens==3
-    assert np.isclose(96.77, transfocator.find_best_combo(318.5).effective_radius, atol=0.1)
-    assert np.isclose(318.5, transfocator.find_best_combo(318.5).image(0.0), atol=0.1)
-    
-    #test with xrt[1] and tfs[1]
-    assert transfocator.find_best_combo(367.9).nlens==2
-    assert np.isclose(187.5, transfocator.find_best_combo(367.9).effective_radius,atol=0.1)
-    assert np.isclose(367.9, transfocator.find_best_combo(367.9).image(0.0),atol=0.1)
-    
+    # A solution with a prefocus
+    combo = transfocator.find_best_combo(312.5)
+    assert combo.nlens == 2
+    assert np.isclose(312.5, combo.image(0.0), atol=0.1)
+    # A solution where there are no valid prefocus
+    transfocator.xrt_limit._read_pv.put(1500.)
+    combo = transfocator.find_best_combo(target=302.5)
+    assert combo.nlens == 1
+    assert np.isclose(302.5, combo.image(0.0), atol=0.1)
 
-    #test xrt[1] and tfs[0]
-    assert transfocator.find_best_combo(305.35).nlens==2
-    assert np.isclose(187.5, transfocator.find_best_combo(305.35).effective_radius, atol=0.1)
-    assert np.isclose(305.35, transfocator.find_best_combo(305.35).image(0.0), atol=0.1)
 
-    #test xrt[1] and tfs[0,1,2,3]
-    assert transfocator.find_best_combo(356.48, n=5).nlens==5
-    assert np.isclose(69.76, transfocator.find_best_combo(356.48, n=5).effective_radius, atol=0.1)
-    assert np.isclose(356.48, transfocator.find_best_combo(356.48, n=5).image(0.0), atol=0.1)
-
+@using_fake_epics_pv
 def test_transfocator_focus_at(transfocator):
     #test with tfs[0] and xrt[0]
-    transfocator.focus_at(312.5)
-    assert [lens.inserted for lens in transfocator.xrt_lenses] == [True, False]
-    assert [lens.inserted for lens in transfocator.tfs_lenses] == [True, False, False, False]
-    
-    #test with xrt[1] and tfs[1,3]
-    transfocator.focus_at(318.5)
-    assert [lens.inserted for lens in transfocator.xrt_lenses] == [False, True]
-    assert [lens.inserted for lens in transfocator.tfs_lenses] == [False, True, False, True]
-
-    #test with xrt[1] and tfs[1]
-    transfocator.focus_at(367.9)
-    assert [lens.inserted for lens in transfocator.xrt_lenses] == [False, True]
-    assert [lens.inserted for lens in transfocator.tfs_lenses] == [False, True, False, False]
-    
-    #test xrt[1] and tfs[0]
-    transfocator.focus_at(305.35)
-    assert [lens.inserted for lens in transfocator.xrt_lenses] == [False, True]
-    assert [lens.inserted for lens in transfocator.tfs_lenses] == [True, False, False, False]
-    
-    #test xrt[1] and tfs[0,1,2,3]
-    transfocator.focus_at(356.48, n=5)
-    assert [lens.inserted for lens in transfocator.xrt_lenses] == [False, True]
-    assert [lens.inserted for lens in transfocator.tfs_lenses] == [True, True, True, True]
-
-
+    # Insert Transfocator lenses so we can test that they are properly removed
+    for lens in transfocator.tfs_lenses:
+        insert(lens)
+    transfocator.focus_at(value=312.5, wait=False)
+    assert transfocator.prefocus_bot._insert._write_pv.get() == 1
+    assert transfocator.tfs_02._insert._write_pv.get() == 1
+    for lens in transfocator.tfs_lenses:
+        if lens != transfocator.tfs_02:
+            assert lens._remove._write_pv.get() == 1
