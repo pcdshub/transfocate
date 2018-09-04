4d8b099e0e221c826515b06fca1943b8f17bf136
Travis CI User <travis@example.org>
2018-09-04 23:32:11 +0000

    Update docs after building Travis build 43 of pcdshub/transfocate
    The docs were built from the branch 'master' against the commit
    12dba7d2f18ef5289443b1b36478ffa3025ed713.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pcdshub/transfocate/jobs/424567272.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1+6.g9e567a1',
+    VERSION: '0.3.2+3.g12dba7d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
