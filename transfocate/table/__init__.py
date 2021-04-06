7f177877fa6eded1d913a3aceeb6da441700d219
Ken Lauer <klauer@slac.stanford.edu>
2021-04-06 13:16:59 -0700

    ENH: bring in table generation/loading utilities from ioc

---
+++ transfocate/table/__init__.py
@@ -0,0 +1,6 @@
+from .info import data, read_spreadsheet
+from .report import generate_report
+from .ioc import generate_header as create_ioc_header
+from .plc_table import generate_source as create_plc_code
+
+__all__ = ["generate_report", "data", "read_spreadsheet", "create_ioc_header", "create_plc_code"]
