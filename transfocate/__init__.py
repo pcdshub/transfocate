2331bbe1452b864cb07a5285ac64ed417339a3d9
Ken Lauer <klauer@slac.stanford.edu>
2023-03-07 14:06:30 -0800

    BLD: migrate to setuptools-scm
    Performed by pcds-migration-tools SetuptoolsScmMigration

--- transfocate/__init__.py
+++ transfocate/__init__.py
@@ -1,9 +1,6 @@
+from .version import __version__  # noqa: F401
 __all__ = ['Lens', 'LensConnect', 'Calculator', 'Transfocator']
 
 from .lens import Lens, LensConnect
 from .calculator import Calculator
 from .transfocator import Transfocator
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
